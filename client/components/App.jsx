3c9aae91f7be2fdb2fb41c05f2bbb92564f978a6
Logan Rosenlund <loganrosenlund61@gmail.com>
2022-03-25 19:06:20 -0700

    User is shown login page if they are not logged in.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,16 +6,39 @@ import NavBar from './NavBar';
 import Home from './Home';
 import Prompts from './Prompts';
 import Pictures from './Pictures';
+import LoginPage from './LoginPage';
+import AuthContext from './auth-context';
+
 
 function App() {
+  const [authStatus, setAuthStatus] = useState(localStorage.getItem('logged-in')); 
+  const login = () => {
+    setAuthStatus(true);
+    localStorage.setItem('logged-in', true);
+  };
+  const logout = () => {
+    setAuthStatus(false);
+    localStorage.removeItem('logged-in');
+  };
+
+  // if(!authStatus) return <LoginPage login={login} />;
+
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="prompts" element={<Prompts />} />
-        <Route path="pictures" element={<Pictures />} />
-      </Routes>
+      <AuthContext.Provider value={{ status: authStatus, login: login, logout: logout }}>
+        {
+          authStatus 
+          ? <>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="prompts" element={<Prompts />} />
+              <Route path="pictures" element={<Pictures />} />
+            </Routes>
+          </>
+          : <LoginPage />
+        }
+      </AuthContext.Provider>
     </>
   )
 }
