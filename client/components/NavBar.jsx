18adc05b28d49d5ce25e6b794ad17a3fa75bce5c
Logan Rosenlund <loganrosenlund61@gmail.com>
2022-03-25 19:09:31 -0700

    Added logout button that logs the user out.

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 
+import AuthContext from './auth-context';
+
 const NavBar = () => {
+  const auth = useContext(AuthContext);
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="/#/">Home</Nav.Link>
-            <Nav.Link href="#/prompts">Prompts</Nav.Link>
-            <Nav.Link href="#/pictures">Pictures</Nav.Link>
-          </Nav>
-        </Container>
+        <Nav className="ms-2 me-auto">
+          <Nav.Link href="/#/">Home</Nav.Link>
+          <Nav.Link href="#/prompts">Prompts</Nav.Link>
+          <Nav.Link href="#/pictures">Pictures</Nav.Link>
+        </Nav>
+        <Nav className='me-2'>
+          <Nav.Link href="/#/" className="ms-auto" onClick={auth.logout}>Logout</Nav.Link>
+        </Nav>
       </Navbar>
     </>
   );
